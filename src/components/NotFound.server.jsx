c8449d8b0ff771dee308829e5d43edd979b802f1
Cody Nguyen <cody@simicart.com>
2021-11-19 14:57:19 +0700

    Temp no serverside rendering for pb until removed htmlparser

--- src/components/NotFound.server.jsx
+++ src/components/NotFound.server.jsx
@@ -1,7 +1,7 @@
 import {useLocation, Link, useHistory} from 'react-router-dom';
 
 import Layout from './Layout.server';
-import {PageBuilderComponent} from 'simi-pagebuilder-react';
+//import {PageBuilderComponent} from 'simi-pagebuilder-react';
 const endPoint = 'https://tapita.io/pb/graphql/';
 const integrationToken = '14FJiubdB8n3Byig2IkpfM6OiS6RTO801622446444';
 
@@ -16,10 +16,10 @@ export default function NotFound({country = {isoCode: 'US'}}) {
     endPoint,
     integrationToken,
   };
-  if (location && location.pathname && pagebuilderUrl[location.pathname]) {
-      const pageData = pagebuilderUrl[location.pathname];
-    return <Layout><div className="server-pb-ctn"><PageBuilderComponent key={location.pathname} {...pbcProps} pageData={pageData} maskedId={pageData.masked_id} /></div></Layout>;
-  }
+//   if (location && location.pathname && pagebuilderUrl[location.pathname]) {
+//       const pageData = pagebuilderUrl[location.pathname];
+//     return <Layout><div className="server-pb-ctn"><PageBuilderComponent key={location.pathname} {...pbcProps} pageData={pageData} maskedId={pageData.masked_id} /></div></Layout>;
+//   }
   return (
     <Layout>
       <NotFoundClient />
